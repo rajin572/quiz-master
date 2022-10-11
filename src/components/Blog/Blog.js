767e51a773805f15cf6f75fe4d8f0324fd7306bc
Din Islam Rajin <dirrazin572@gmail.com>
2022-10-11 23:26:41 +0600

    added blog section

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import './Blog.css'
 
 const Blog = () => {
     return (
-        <div>
-            I am blog
+        <div className='blogQuestion'>
+            <h1>Question And Answer</h1>
+            <section class="blogAnswerSection">
+                <div class="card">
+                    <h2>Question 1: </h2>
+                    <h3><span>Answer : </span></h3>
+                    <p>
+                    </p>
+                </div>
+                <div class="card">
+                    <h2>Question 2: </h2>
+                    <h3><span>Answer : </span></h3>
+                    <p></p>
+                </div>
+                <div class="card">
+                    <h2>Question 3: </h2>
+                    <h3><span>Answer : </span>
+                    </h3>
+                    <p></p>
+                </div>
+            </section> 
         </div>
     );
 };
